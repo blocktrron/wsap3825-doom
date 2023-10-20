6537d2bddd1aa77c67878654b696da252eb786cc
David Bauer <mail@david-bauer.net>
2023-10-20 13:48:42 +0200

    board: use more descriptive image filename

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Boot
 
 ```
-setenv serverip 192.168.1.66; setenv ipaddr 192.168.1.6; tftpboot 0x3000000 uImage; bootm 0x3000000
+setenv serverip 192.168.1.66; setenv ipaddr 192.168.1.6; tftpboot 0x3000000 wsap3825-doom-uimage.bin; bootm 0x3000000
 ```
 
 ## Autoboot
--- board/wsap3825i/mkimage.sh
+++ board/wsap3825i/mkimage.sh
@@ -2,4 +2,13 @@
 
 IMAGE_DIR=$1
 
-mkimage -A powerpc -O linux -T kernel -C none -e 0x2000000 -a 0x2000000 -n Linux -d $IMAGE_DIR/simpleImage.ws-ap3825i $IMAGE_DIR/uImage
+mkimage \
+	-A powerpc \
+	-O linux \
+	-T kernel \
+	-C none \
+	-e 0x2000000 \
+	-a 0x2000000 \
+	-n "DOOM_WS-AP3825i" \
+	-d $IMAGE_DIR/simpleImage.ws-ap3825i \
+	$IMAGE_DIR/wsap3825-doom-uimage.bin
